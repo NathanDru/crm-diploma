fe28d7aaaa725813e15079f77d062fc0a56da7f0
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-06-11 23:51:46 +0300

    simle react app was set up

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'boot.js'),
+
+    output: {
+        path: path.join(__dirname, 'public', 'build'),
+        filename: 'app.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react'],
+                },
+            },
+        ]
+    },
+
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx'],
+    },
+
+    devtool: 'source-map',
+};
