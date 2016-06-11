8d71244246cbd474cf355934b61fe01b4093b5e5
Никита Древицкий < >
2016-06-11 23:46:02 +0300

    Added GET fucntion for tables

--- src/server/controllers.js
+++ src/server/controllers.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var dataAcessors = require('./dataAcessors');
 var Clients = new dataAcessors.Clients();
+var Managers = new dataAcessors.Managers();
+var Sources = new dataAcessors.Sources();
+var Ranks = new dataAcessors.Ranks();
+var Statuses = new dataAcessors.Statuses();
+var Orders = new dataAcessors.Orders();
+var ServiceCategories = new dataAcessors.ServiceCategories();
+var ProductCategories = new dataAcessors.ProductCategories();
+var Providers = new dataAcessors.Providers();
+var Products = new dataAcessors.Products();
+var Services = new dataAcessors.Services();
 
 module.exports = function router(params) {
     var router = express.Router();
@@ -9,10 +19,10 @@ module.exports = function router(params) {
         .get((request, response) => Clients.getAll()
             .then(result => response.json(result))
             .catch(err => response.send(err)))
-        .post((request, response) => Clients.create()
-            .then(result => response.json(result))
-            .catch(err => response.send(err)));
-          
+    // .post((request, response) => Clients.create()
+    //     .then(result => response.json(result))
+    //     .catch(err => response.send(err)));
+
     router.route('/clients/:client_id')
         .get((request, response) => Clients.getById(request.params.client_id)
             .then(result => response.json(result))
@@ -22,8 +32,107 @@ module.exports = function router(params) {
         //     .catch(err => response.send(err)))
         .delete((request, response) => Clients.delete(request.params.client_id)
             .then(result => response.json(result))
-            .catch(err => response.send(err)))
+            .catch(err => response.send(err)));
 
+    router.route('/managers')
+        .get((request, response) => Managers.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/managers/:manager_id')
+        .get((request, response) => Managers.getById(request.params.manager_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/sources')
+        .get((request, response) => Sources.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/sources/:source_id')
+        .get((request, response) => Sources.getById(request.params.source_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/ranks')
+        .get((request, response) => Ranks.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/ranks/:rank_id')
+        .get((request, response) => Ranks.getById(request.params.rank_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/statuses')
+        .get((request, response) => Statuses.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/statuses/:status_id')
+        .get((request, response) => Statuses.getById(request.params.status_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/orders')
+        .get((request, response) => Orders.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/orders/:order_id')
+        .get((request, response) => Orders.getById(request.params.order_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/serviceCategories')
+        .get((request, response) => ServiceCategories.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/serviceCategories/:category_id')
+        .get((request, response) => ServiceCategories.getById(request.params.category_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/productCategories')
+        .get((request, response) => ProductCategories.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/productCategories/:category_id')
+        .get((request, response) => ProductCategories.getById(request.params.category_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/providers')
+        .get((request, response) => Providers.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/providers/:provider_id')
+        .get((request, response) => Providers.getById(request.params.provider_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/products')
+        .get((request, response) => Products.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/products/:product_id')
+        .get((request, response) => Products.getById(request.params.product_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/services')
+        .get((request, response) => Services.getAll()
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
+
+    router.route('/services/:service_id')
+        .get((request, response) => Services.getById(request.params.service_id)
+            .then(result => response.json(result))
+            .catch(err => response.send(err)));
 
 
     return router;
--- src/server/dataAcessors.js
+++ src/server/dataAcessors.js
@@ -69,5 +69,234 @@ class Clients {
     }
 }
 
+class Managers {
+    constructor() {
+        this.crud = new CRUDAPI('Managers');
+        this.idField = 'ID_manager';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(manager) {
+        return this.crud.create(manager);
+    }
+    update(manager){
+       //TODO implement
+    }
+}
+
+class Sources {
+    constructor() {
+        this.crud = new CRUDAPI('Sources');
+        this.idField = 'Source_ID';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(source) {
+        return this.crud.create(source);
+    }
+    update(source){
+       //TODO implement
+    }
+}
+
+class Ranks {
+    constructor() {
+        this.crud = new CRUDAPI('Ranks');
+        this.idField = 'Rank_ID';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(rank) {
+        return this.crud.create(rank);
+    }
+    update(rank){
+       //TODO implement
+    }
+}
+
+class Statuses {
+    constructor() {
+        this.crud = new CRUDAPI('Statuses');
+        this.idField = 'Status_ID';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(status) {
+        return this.crud.create(status);
+    }
+    update(status){
+       //TODO implement
+    }
+}
+
+class Orders {
+    constructor() {
+        this.crud = new CRUDAPI('Orders');
+        this.idField = 'ID_order';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(order) {
+        return this.crud.create(order);
+    }
+    update(order){
+       //TODO implement
+    }
+}
+
+class ServiceCategories {
+    constructor() {
+        this.crud = new CRUDAPI('Service_categories');
+        this.idField = 'ID_category';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(category) {
+        return this.crud.create(category);
+    }
+    update(category){
+       //TODO implement
+    }
+}
+
+class ProductCategories {
+    constructor() {
+        this.crud = new CRUDAPI('Product_categories');
+        this.idField = 'ID_category';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(category) {
+        return this.crud.create(category);
+    }
+    update(category){
+       //TODO implement
+    }
+}
+
+class Providers {
+    constructor() {
+        this.crud = new CRUDAPI('Providers');
+        this.idField = 'Provider_ID';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(provider) {
+        return this.crud.create(provider);
+    }
+    update(provider){
+       //TODO implement
+    }
+}
+
+class Products {
+    constructor() {
+        this.crud = new CRUDAPI('Products');
+        this.idField = 'ID_product';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(product) {
+        return this.crud.create(product);
+    }
+    update(product){
+       //TODO implement
+    }
+}
+
+class Services {
+    constructor() {
+        this.crud = new CRUDAPI('Services');
+        this.idField = 'ID_service';
+    }
+    getAll() {
+        return this.crud.readAll();
+    }
+    getById(id) {
+        return this.crud.readBy(this.idField, id);
+    }
+    delete(id) {
+        return this.crud.deleteBy(this.idField, id);
+    }
+    create(service) {
+        return this.crud.create(service);
+    }
+    update(service){
+       //TODO implement
+    }
+}
 
 module.exports.Clients = Clients;
+module.exports.Managers = Managers;
+module.exports.Sources = Sources;
+module.exports.Ranks = Ranks;
+module.exports.Statuses = Statuses;
+module.exports.Orders = Orders;
+module.exports.ServiceCategories = ServiceCategories;
+module.exports.ProductCategories = ProductCategories;
+module.exports.Providers = Providers;
+module.exports.Products = Products;
+module.exports.Services = Services;
